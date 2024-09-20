bf6127d2336392fd85d8d59ca391bb65b9665bc7
petitavo <Gustavohuanca1020@gmail.com>
2024-09-20 08:25:51 -0500

    feat(app): http client added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './public/sidenav/sidenav.component';
+import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent],
+  imports: [RouterOutlet, SidenavComponent, ClientsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
