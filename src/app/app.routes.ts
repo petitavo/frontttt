fcaaac475d96d2b97766e2c066b0d7d302a51811
petitavo <Gustavohuanca1020@gmail.com>
2024-09-21 12:52:50 -0500

    feat(inventory): inventory route added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
+import {InventoryComponent} from "./GrapeFlow/producer/components/inventory/inventory.component";
 
 export const routes: Routes = [
   {path: 'clients', component : ClientsComponent},
+  {path: 'inventory', component : InventoryComponent},
 ];
