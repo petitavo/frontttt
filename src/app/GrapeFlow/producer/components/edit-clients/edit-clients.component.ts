6c31604dfc320892c8e7905018f42eda7e226795
AngeloC12 <angelocuri@hotmail.com>
2024-09-21 21:56:44 -0500

    feat: edit client component added

---
+++ src/app/GrapeFlow/producer/components/edit-clients/edit-clients.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-clients',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-clients.component.html',
+  styleUrl: './edit-clients.component.css'
+})
+export class EditClientsComponent {
+
+}
