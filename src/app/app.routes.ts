19627cdb7353b3dbc070bcf6667d7305f15ef12a
petitavo <Gustavohuanca1020@gmail.com>
2024-09-22 07:16:22 -0500

    feat(lote): lote route added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
 import {InventoryComponent} from "./GrapeFlow/producer/components/inventory/inventory.component";
+import {LoteComponent} from "./GrapeFlow/producer/components/lote/lote.component";
 
 export const routes: Routes = [
   {path: 'clients', component : ClientsComponent},
   {path: 'inventory', component : InventoryComponent},
+  {path: 'lote', component: LoteComponent}
 ];
