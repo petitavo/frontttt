7bddc4892f32dab6925376444a0ef88e8ebd454e
petitavo <Gustavohuanca1020@gmail.com>
2024-09-22 07:15:33 -0500

    feat(lote): lote service configured.

--- src/app/GrapeFlow/producer/services/lote.service.ts
+++ src/app/GrapeFlow/producer/services/lote.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BaseService} from "../../../shared/services/base.service"; // Ruta hacia tu BaseService
+import { Lote } from '../model/lote.entity'; // Asegúrate de usar el modelo correcto de Lote
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoteService {
+export class LoteService extends BaseService<Lote> {
 
-  constructor() { }
+  constructor() {
+    super();
+    this.resourceEndPoint = '/lotes'; // Asegúrate de usar el endpoint correcto
+  }
 }
