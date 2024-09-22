946d30f3941f8080656074b22e225fe9549f98fc
petitavo <Gustavohuanca1020@gmail.com>
2024-09-22 06:00:19 -0500

    feat(lote): lote component added.

---
+++ src/app/GrapeFlow/producer/components/lote/lote.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lote',
+  standalone: true,
+  imports: [],
+  templateUrl: './lote.component.html',
+  styleUrl: './lote.component.css'
+})
+export class LoteComponent {
+
+}
