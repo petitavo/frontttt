9bc4c6992a0ac4a532e9342602e63b1423053cea
PieroSL <81635582+PieroSL@users.noreply.github.com>
2024-09-22 00:22:40 -0500

    add into db.json order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './public/sidenav/sidenav.component';
 import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
+import {DetailOrderComponent} from "./GrapeFlow/producer/components/detail-order/detail-order.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, ClientsComponent],
+  imports: [RouterOutlet, SidenavComponent, ClientsComponent, DetailOrderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
