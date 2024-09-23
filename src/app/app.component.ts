a354833283146bb894a7839f033b70fdeaf9f160
petitavo <Gustavohuanca1020@gmail.com>
2024-09-23 15:35:49 -0500

    feat(sidenav): consumer sidenav added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './public/sidenav/sidenav.component';
 import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
 import {LoteComponent} from "./GrapeFlow/producer/components/lote/lote.component";
+import {SidenavConsumerComponent} from "./public/sidenav-consumer/sidenav-consumer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, ClientsComponent, LoteComponent],
+  imports: [RouterOutlet, SidenavComponent, ClientsComponent, LoteComponent, SidenavConsumerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
