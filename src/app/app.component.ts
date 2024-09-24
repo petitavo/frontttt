0df098cee063f732cdc5c975de25989bd82f89aa
petitavo <Gustavohuanca1020@gmail.com>
2024-09-24 10:16:29 -0500

    feat(inventory): inventory component fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { SidenavComponent } from './public/sidenav/sidenav.component';
 import {ClientsComponent} from "./GrapeFlow/producer/components/clients/clients.component";
 import {LoteComponent} from "./GrapeFlow/producer/components/lote/lote.component";
 import {SidenavConsumerComponent} from "./public/sidenav-consumer/sidenav-consumer.component";
+import {InventoryComponent} from "./GrapeFlow/producer/components/inventory/inventory.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, ClientsComponent, LoteComponent, SidenavConsumerComponent],
+  imports: [RouterOutlet, SidenavComponent, ClientsComponent, LoteComponent, SidenavConsumerComponent, InventoryComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
